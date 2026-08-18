4f373dc5ada4b527e49d964224aae28fff1350fa
royrarararyan-netizen <royrarararyan@gmail.com>
2026-08-18 23:41:12 +0900

    Add Ssgbuni app icon and install files

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE = 'ssgbuni-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
+
